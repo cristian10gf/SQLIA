2f472fc49174e4faf6949192eb92188e7b4e3d50
cristian gonzalez franco <97616425+cristian10gf@users.noreply.github.com>
2026-05-26 20:09:22 -0500

    Update routing for evaluations and challenges in App component
    - Replaced `EvaluationsAndChallengesPage` with `EvaluationsListPage` for better clarity in navigation.
    - Added a new route for `EvaluationDetailPage` to handle specific evaluation details.
    - Ensured consistent routing structure for evaluation-related pages, enhancing user experience.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,8 @@ import { LoginPage } from './features/auth/presentation/pages/LoginPage';
 import { RegisterPage } from './features/auth/presentation/pages/RegisterPage';
 import { DashboardPage } from './features/dashboard/presentation/pages/DashboardPage';
 import { CoursesPage } from './features/courses/presentation/pages/CoursesPage';
-import EvaluationsAndChallengesPage from './features/evaluationsAndChallenges/presentation/pages/EvaluationsAndChallengesPage';
+import EvaluationsListPage from './features/evaluationsAndChallenges/presentation/pages/EvaluationsListPage';
+import EvaluationDetailPage from './features/evaluationsAndChallenges/presentation/pages/EvaluationDetailPage';
 import { ResultsPage } from './features/results/presentation/pages/ResultsPage';
 import ReportsPage from './features/reports/presentation/pages/ReportsPage';
 import ManagementPage from './features/managemen/presentation/pages/ManagemenPage';
@@ -20,12 +21,17 @@ function App() {
       <Route path="/results" element={<ResultsPage />} />
       <Route path="/courses" element={<CoursesPage />} />
       <Route path="/evaluation" element={<ResultsPage />} />
-       <Route path="/reports" element={<ReportsPage />} />
-       <Route path="/managemen" element={<ManagementPage />} />
+      <Route path="/reports" element={<ReportsPage />} />
+      <Route path="/managemen" element={<ManagementPage />} />
 
       <Route
         path="/courses/evaluations-challenges/:courseId"
-        element={<EvaluationsAndChallengesPage />}
+        element={<EvaluationsListPage />}
+      />
+
+      <Route
+        path="/courses/evaluations-challenges/:courseId/evaluation/:evaluationId"
+        element={<EvaluationDetailPage />}
       />
 
       <Route path="*" element={<Navigate to="/login" replace />} />
